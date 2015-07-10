d436c4dc053d8e690179fe8dafdccef86d5f1fd8
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-07-10 00:32:02 -0400

    Add comments and documentation to definitions.

--- pkg/db/schema/schema.go
+++ pkg/db/schema/schema.go
@@ -2,27 +2,33 @@ package schema
 
 import "time"
 
+// A Service is a named service that has a set of templates associated with it.
 type Service struct {
 	ID      string    `gorethink:"id"`
 	Name    string    `gorethink:"name"`
-	Type    string    `gorethink:"_type"`
-	Current bool      `gorethink:"current"`
+	Type    string    `gorethink:"_type"`   // The type of service.
+	Current bool      `gorethink:"current"` // Is this the current service definition.
 	CTime   time.Time `gorethink:"ctime"`
 }
 
+// Service2Template is a join table that maps a service to templates.
 type Service2Template struct {
 	ID         string `gorethink:"id"`
 	ServiceID  string `gorethink:"service_id"`
 	TemplateID string `gorethink:"template_id"`
 }
 
+// Template is a consul-template for service configuration/monitoring.
 type Template struct {
 	ID      string `gorethink:"id"`
 	Name    string `gorethink:"name"`
-	Body    string `gorethink:"body"`
-	Version int    `gorethink:"version"`
+	Path    string `gorethink:"path"`    // Path to template definition
+	Body    string `gorethink:"body"`    // The body of the template (same as Path at point in time)
+	Version int    `gorethink:"version"` // Version of template. A template can change over time.
 }
 
+// ChangeLog tracks the changes to service and template definitions. It allows
+// a user to see the difference across definitions.
 type ChangeLog struct {
 	ID      string    `gorethink:"id"`
 	OtherID string    `gorethink:"other_id"`
